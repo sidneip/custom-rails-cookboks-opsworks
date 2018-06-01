08c1d43fa0cef018e67b9ace616d085ccabaab90
Sidnei Pacheco <sidneip.junior@gmail.com>
2018-06-01 14:41:09 -0300

    dotenv

--- dotenv/definitions/dotenv_template.rb
+++
@@ -1,9 +0,0 @@
-define :dotenv_template do
-  template "#{params[:deploy][:deploy_to]}/shared/config/dotenv" do
-    source "env.erb"
-    owner params[:deploy][:user]
-    group params[:deploy][:group]
-    mode "0660"
-    variables :env => params[:env]
-  end
-end
--- dotenv/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'dotenv'
-maintainer       'Degica'
-maintainer_email 'rramsden@degica.com'
-license          'All rights reserved'
-description      'Creates a .env file from custom JSON in Opsworks'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-depends          'deploy'
