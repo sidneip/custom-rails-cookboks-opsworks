6a413f5ba5e977b30f4a060c2865d96d02cc8ce5
Sidnei Pacheco <sidneip.junior@gmail.com>
2018-06-05 15:26:13 -0300

    paper

--- opsworks_papertrail/attributes/default.rb
+++
@@ -1,2 +0,0 @@
-default[:papertrail] = {}
-default[:papertrail][:log_files] = {}
--- opsworks_papertrail/recipes/rsyslog_service.rb
+++
@@ -1,5 +0,0 @@
-service "rsyslog" do
-  supports :restart => true
-  restart_command "/etc/init.d/rsyslog restart"
-  action :nothing
-end
