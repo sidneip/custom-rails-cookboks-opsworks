c5b20417dff193b018e81e3ec79da79be917652f
Sidnei Pacheco <sidneip.junior@gmail.com>
2018-06-06 17:47:18 -0300

    env

--- opsworks-dotenv/recipes/dotenv.rb
+++ opsworks-dotenv/recipes/dotenv.rb
@@ -14,18 +14,19 @@
 require 'shellwords'
 node[:deploy].each do |application, deploy|
   rails_env = deploy[:rails_env]
-
-  Chef::Log.info("Generating dotenv for app: #{application} with env: #{rails_env}...")
   
-  environment_variables = deploy[:app_env].to_h.merge(deploy[:environment_variables].to_h)
-
-  directory "#{deploy[:deploy_to]}/shared" do
-    mode 0755
-    recursive true
-    owner deploy[:user]
+  opsworks_deploy_dir do
+    user deploy[:user]
     group deploy[:group]
-    action :create
+    path deploy[:deploy_to]
   end
+
+  opsworks_rails do
+    deploy_data deploy
+    app application
+  end
+
+  environment_variables = deploy[:app_env].to_h.merge(deploy[:environment_variables].to_h)
 
   file("#{deploy[:deploy_to]}/shared/.env") do
     mode '0755'
